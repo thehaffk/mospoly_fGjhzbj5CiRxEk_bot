5ebe2602d7429f98bdf2919bad308f82c40d5158
Felix Haffk <root@komar>
2024-06-03 04:11:11 +0300

    Implemented some things for university project

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,6 +3,7 @@ import os
 import logging
 from datetime import datetime, timedelta
 from aiogram import Bot, Dispatcher, executor, types
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from dotenv import load_dotenv
 import database_handler as db
 
@@ -44,9 +45,9 @@ async def on_start(message: types.Message):
 @dp.message_handler(commands=['help'])
 async def on_help(message: types.Message):
     await message.answer(
-        '/add <описание> <дата и время> - добавить задачу\n'
+        '/add <описание> <время, используя h, m и d> - добавить задачу\n'
         '/tasks - посмотреть задачи\n/task <id> - посмотреть задачу\n'
-        '/edit <id> <новое описание> <новая дата и время> - редактировать задачу\n'
+        '/edit <id> <новое описание> <новое относительное время> - редактировать задачу\n'
         '/delete <id> - удалить задачу'
     )
 
@@ -54,13 +55,14 @@ async def on_help(message: types.Message):
 @dp.message_handler(commands=['add'])
 async def on_add(message: types.Message):
     try:
+        user_id = message.from_user.id
         args = message.text.split()[1:]
         description = ' '.join(args[:-1])
         due_date = parse_time(args[-1])
         if due_date <= 0:
             return await message.answer("Неправильный формат времени, используйте `1h` или `7d`")
         due_datetime = datetime.now() + timedelta(minutes=due_date)
-        await db.add_task(description, due_datetime.strftime('%Y-%m-%d %H:%M'))
+        await db.add_task(user_id, description, due_datetime.strftime('%Y-%m-%d %H:%M'))
         await message.answer('Задача добавлена!')
     except Exception as e:
         logging.error(e)
@@ -69,19 +71,21 @@ async def on_add(message: types.Message):
 
 @dp.message_handler(commands=['tasks'])
 async def on_tasks(message: types.Message) -> None:
-    tasks = await db.get_tasks()
-    tasks_text = '\n'.join([f"{task[0]}: {task[1]} - {task[2]}" for task in tasks])
+    user_id = message.from_user.id
+    tasks = await db.get_tasks(user_id)
+    tasks_text = '\n'.join([f"{task[0]}: {task[2]} - {task[3]}" for task in tasks])
     await message.answer(f"Задачи:\n{tasks_text}")
 
 
 @dp.message_handler(commands=['task'])
 async def on_task(message: types.Message) -> None:
     try:
+        user_id = message.from_user.id
         args = message.text.split()[1:]
         task_id = int(args[0])
-        task = await db.get_task(task_id)
+        task = await db.get_task(user_id, task_id)
         if task:
-            await message.answer(f"Задача {task[0]}: {task[1]} - {task[2]}")
+            await message.answer(f"Задача {task[0]}: {task[2]} - {task[3]}")
         else:
             await message.answer(f"Задача с ID {task_id} не найдена.")
     except Exception as e:
@@ -92,12 +96,13 @@ async def on_task(message: types.Message) -> None:
 @dp.message_handler(commands=['edit'])
 async def on_edit(message: types.Message) -> None:
     try:
+        user_id = message.from_user.id
         args = message.text.split()[1:]
         task_id = int(args[0])
         new_description = ' '.join(args[1:-1])
         new_due_date = args[-1]
         due_datetime = datetime.now() + timedelta(minutes=parse_time(new_due_date))
-        await db.edit_task(task_id, new_description, due_datetime.strftime('%Y-%m-%d %H:%M'))
+        await db.edit_task(user_id, task_id, new_description, due_datetime.strftime('%Y-%m-%d %H:%M'))
         await message.answer(f"Задача {task_id} отредактирована.")
     except Exception as e:
         logging.error(e)
@@ -107,17 +112,31 @@ async def on_edit(message: types.Message) -> None:
 @dp.message_handler(commands=['delete'])
 async def on_delete(message: types.Message) -> None:
     try:
+        user_id = message.from_user.id
         args = message.text.split()[1:]
         task_id = int(args[0])
-        await db.delete_task(task_id)
+        await db.delete_task(user_id, task_id)
         await message.answer(f"Задача {task_id} удалена.")
     except Exception as e:
         logging.error(e)
         await message.answer('Ошибка при удалении задачи.')
 
 
+async def notify_due_tasks():
+    tasks = await db.get_due_tasks()
+    for task in tasks:
+        _user_id, description, due_date, _task_id = task
+        await bot.send_message(_user_id, f"Задача c описанием "
+                                         f"\n```'{description}'```\n"
+                                         f" должна быть выполнена до {due_date}!")
+        await db.delete_task(user_id=_user_id, task_id=_task_id)
+
+
 async def main():
     await db.init_db()
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(notify_due_tasks, 'interval', minutes=1)
+    scheduler.start()
     await dp.start_polling()
 
 
--- bot/database_handler.py
+++ bot/database_handler.py
@@ -1,40 +1,58 @@
+import datetime
+
 import aiosqlite
+
 
 async def init_db():
     async with aiosqlite.connect('tasks.db') as db:
         await db.execute('''
             CREATE TABLE IF NOT EXISTS tasks (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
                 description TEXT NOT NULL,
                 due_date TEXT NOT NULL
             )
         ''')
         await db.commit()
 
-async def add_task(description, due_date):
+
+async def add_task(user_id, description, due_date):
     async with aiosqlite.connect('tasks.db') as db:
-        await db.execute('INSERT INTO tasks (description, due_date) VALUES (?, ?)', (description, due_date))
+        await db.execute('INSERT INTO tasks (user_id, description, due_date) VALUES (?, ?, ?)',
+                         (user_id, description, due_date))
         await db.commit()
 
-async def get_tasks():
+
+async def get_tasks(user_id):
     async with aiosqlite.connect('tasks.db') as db:
-        cursor = await db.execute('SELECT * FROM tasks')
+        cursor = await db.execute('SELECT * FROM tasks WHERE user_id = ?', (user_id,))
         tasks = await cursor.fetchall()
         return tasks
 
-async def get_task(task_id):
+
+async def get_task(user_id, task_id):
     async with aiosqlite.connect('tasks.db') as db:
-        cursor = await db.execute('SELECT * FROM tasks WHERE id = ?', (task_id,))
+        cursor = await db.execute('SELECT * FROM tasks WHERE id = ? AND user_id = ?', (task_id, user_id))
         task = await cursor.fetchone()
         return task
 
-async def edit_task(task_id, new_description, new_due_date):
+
+async def edit_task(user_id, task_id, new_description, new_due_date):
     async with aiosqlite.connect('tasks.db') as db:
-        await db.execute('UPDATE tasks SET description = ?, due_date = ? WHERE id = ?',
-                         (new_description, new_due_date, task_id))
+        await db.execute('UPDATE tasks SET description = ?, due_date = ? WHERE id = ? AND user_id = ?',
+                         (new_description, new_due_date, task_id, user_id))
         await db.commit()
 
-async def delete_task(task_id):
+
+async def delete_task(user_id, task_id):
     async with aiosqlite.connect('tasks.db') as db:
-        await db.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
+        await db.execute('DELETE FROM tasks WHERE id = ? AND user_id = ?', (task_id, user_id))
         await db.commit()
+
+
+async def get_due_tasks():
+    async with aiosqlite.connect('tasks.db') as db:
+        cursor = await db.execute('SELECT user_id, description, due_date, id FROM tasks WHERE due_date = ?',
+                                  (datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),))
+        tasks = await cursor.fetchall()
+        return tasks
